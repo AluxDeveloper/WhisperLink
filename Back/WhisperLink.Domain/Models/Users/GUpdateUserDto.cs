030cabccb7f72f9884cc0183582966ac69bed403
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-27 01:14:58 +0300

    UpdateUserDto am adaugat Handle, Bio, JobRole

--- Back/WhisperLink.Domain/Models/Users/GUpdateUserDto.cs
+++ Back/WhisperLink.Domain/Models/Users/GUpdateUserDto.cs
@@ -1,11 +1,11 @@
 ﻿namespace WhisperLink.Domain.Models.Users
 {
-    // DTO pentru actualizare profil utilizator
     public class UpdateUserDto
     {
-        // Câmpuri opționale - doar ce vrea user-ul să modifice
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Handle { get; set; }
+        public string? Bio { get; set; }
+        public string? JobRole { get; set; }
         public string? ProfilePictureUrl { get; set; }
     }
 }
