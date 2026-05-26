29ea5173d82aa9457fa6c935a98b93ee747947f6
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-27 01:13:20 +0300

    LoginDto inlocuire UsernameOrEmail cu Email field

--- Back/WhisperLink.Domain/Models/Auth/ALoginDto.cs
+++ Back/WhisperLink.Domain/Models/Auth/ALoginDto.cs
@@ -2,14 +2,11 @@
 
 namespace WhisperLink.Domain.Models.Users
 {
-    // DTO pentru autentificare
     public class LoginDto
     {
-        // Username SAU email
         [Required]
-        public string UsernameOrEmail { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
 
-        // Parola
         [Required]
         public string Password { get; set; } = string.Empty;
     }
