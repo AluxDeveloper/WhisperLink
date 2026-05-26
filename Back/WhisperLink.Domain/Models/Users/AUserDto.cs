3e114bd8e1e52ea34bdb29338302f66890dc33dc
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-27 01:09:22 +0300

    Updatat UserDto cu adaugari de stringuri noi DisplayName, Handle, Bio, JobRole, Presence

--- Back/WhisperLink.Domain/Models/Users/AUserDto.cs
+++ Back/WhisperLink.Domain/Models/Users/AUserDto.cs
@@ -3,17 +3,18 @@ using WhisperLink.Domain.Enums;
 
 namespace WhisperLink.Domain.Models.Users
 {
-    // DTO pentru returnare date user (FĂRĂ parolă)
     public class UserDto
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public string Handle { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? JobRole { get; set; }
+        public string? Bio { get; set; }
         public string? ProfilePictureUrl { get; set; }
         public UserRole Role { get; set; }
-        public bool IsOnline { get; set; }
+        public string Presence { get; set; } = "offline";
         public DateTime? LastSeenAt { get; set; }
         public DateTime CreatedAt { get; set; }
     }
